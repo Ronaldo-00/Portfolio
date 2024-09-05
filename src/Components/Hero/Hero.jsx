de428ec1ce2d99d2144cdb5eed4aa0a791ead3e1
Ronaldo-00 <amuthuraman98@gmail.com>
2024-09-05 11:25:19 +0530

    Hero component updated

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ScrollToTop from "../../Components/ScrollTop/ScrollToTop"
 import {useTypewriter, Cursor} from 'react-simple-typewriter' 
 import './Hero.css' // css file
 import linkedin from "../../Assets/icons/linkedin.png"
@@ -19,36 +20,37 @@ const Hero = () => {
   });
   return (
     <div className="main-hero">
-    <div className="hero">
-      <div className="container-row">
-        <div className="container-left">
-            <h1>I'm a <span>{text}</span> <Cursor/> </h1>
-            <p>I'm MUTHU RAMAN.<br></br> A passionate Front End Developer based in Madurai.</p>
-            <div className="professional-link">
-                <a href='https://www.linkedin.com/in/muthu-raman-58a150284' target="blank"><img src={linkedin} alt='linked-in'></img></a>
-                <a href='https://github.com/Ronaldo-00' target="blank"><img src={github} alt='git-hub'></img></a>
-                <a href='https://www.figma.com/@ronaldo11' target="blank"><img src={figma} alt='figma'></img></a>
-            </div>
+      <div className="hero">
+        <div className="container-row">
+          <div className="container-left">
+              <h1>I'm a <span>{text}</span> <Cursor/> </h1>
+              <p>I'm MUTHU RAMAN.<br></br> A passionate Front End Developer based in Madurai.</p>
+              <div className="professional-link">
+                  <a href='https://www.linkedin.com/in/muthu-raman-58a150284' target="blank"><img src={linkedin} alt='linked-in'></img></a>
+                  <a href='https://github.com/Ronaldo-00' target="blank"><img src={github} alt='git-hub'></img></a>
+                  <a href='https://www.figma.com/@ronaldo11' target="blank"><img src={figma} alt='figma'></img></a>
+              </div>
+          </div>
+          <div className="container-right">
+              <img src={profile} alt='profile-img'></img>
+          </div> 
         </div>
-        <div className="container-right">
-            <img src={profile} alt='profile-img'></img>
-        </div> 
+        <div className="tech-stack">
+          <div className="tech-heading">
+              <h3>Tech Stack | </h3>
+          </div>
+          <div className="tech-img">
+              <img src={html} alt="html" title='html'></img>
+              <img src={css} alt='css' title='css' ></img>
+              <img src={js} alt='js'title='Js'></img>
+              <img src={react} alt='react'title='React'></img>
+              <img src={figma} alt='figma'title='Figma'></img>
+              <img src={node} alt='node'title='Node'></img>
+              <img src={bootstrap} alt='bootstrap'title='Bootstrap'></img>
+          </div>
+        </div>
+        <ScrollToTop></ScrollToTop>
       </div>
-      <div className="tech-stack">
-        <div className="tech-heading">
-            <h3>Tech Stack | </h3>
-        </div>
-        <div className="tech-img">
-            <img src={html} alt="html" title='html'></img>
-            <img src={css} alt='css' title='css' ></img>
-            <img src={js} alt='js'title='Js'></img>
-            <img src={react} alt='react'title='React'></img>
-            <img src={figma} alt='figma'title='Figma'></img>
-            <img src={node} alt='node'title='Node'></img>
-            <img src={bootstrap} alt='bootstrap'title='Bootstrap'></img>
-        </div>
-      </div>
-    </div>
     </div>
   )
 }
